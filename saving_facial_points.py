76340067b7a0f17670772fe95d6213099bbe7a43
rahbs <rahbs@naver.com>
2019-02-18 06:35:00 +0900

    modified [saving_facial_points]

--- saving_facial_points.py
+++ saving_facial_points.py
@@ -2,18 +2,19 @@ import dlib
 import cv2
 import numpy as np
 import csv
-import pandas as pd
 
 import models
 import NonLinearLeastSquares
 import ImageProcessing
 
-
 import FaceRendering
 import utils
 
-
 from os import rename, listdir
+
+import datetime
+import time
+
 
 # 키포인트 인식 모델
 predictor_path = "data/shape_predictor_68_face_landmarks.dat"
@@ -40,27 +41,30 @@ cap_background = cv2.VideoCapture("input/"+backgroundvideo+".mp4") # Video for b
 # cap_background = cv2.VideoCapture(0) # WebCAM for background
 cameraImg = cap_background.read()[1]
 
-
-framecnt = 0
 modelParams = np.zeros(20)
 
-csvfile = open("facial_points/"+backgroundvideo+".csv", "w", newline="\n")
+output_csv_name =backgroundvideo +'_'+ datetime.datetime.now().strftime('%Y%m%d_%H%M%S') + '.avi'
+csvfile = open("facial_points/"+output_csv_name+".csv", "w", newline="")
 while True:
     # 배경으로 사용할 영상의 프레임 이미지 읽기
-    cameraImg = cap_background.read()[1]
+    ret, cameraImg = cap_background.read()
     
+    #영상이 끝나면 반복문 탈출
+    if ret == False:
+        break
+
     # 영상에서 얼굴을 인식하고, 키포인트를 추출함
     shapes2D = utils.getFaceKeypoints(cameraImg, detector, predictor, maxImageSizeForDetection)
-  
+    
     #shapes2D가 none이면 얼굴인식이 안되는 상황,
     #none이 아니면 얼굴인식이 되서 페이셜포인트 찾은 상황
     if shapes2D is not None:
         #얼굴 키포인트를 프린트해봄
-        print(shapes2D)
-        print(type(shapes2D)) #list
-        #CSVfile 로 저장하기
+        print(shapes2D[0]) #shapes2D 타입은 list
+
         csvwriter = csv.writer(csvfile)
-        for row in shapes2D:
+        for row in shapes2D[0]:
             csvwriter.writerow(row)
-    else 
+    else :
+        pass
 csvfile.close()
